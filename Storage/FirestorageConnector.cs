3b46ad4180a4944a6ce136c5b08113865ddfeee2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-22 10:36:50 +0200

    Add FirestorageConnector class to FirebaseManager.Storage
    Introduce FirestorageConnector class implementing IFirestorageConnector.
    Add necessary using directives for Firebase and Google Cloud.
    Initialize Firebase app with settings and credentials in Initialize method.

---
+++ Storage/FirestorageConnector.cs
@@ -0,0 +1,40 @@
+﻿using FirebaseAdmin;
+using FirebaseManager.Firebase;
+using FirebaseManager.Firestore;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using NLog;
+
+namespace FirebaseManager.Storage
+{
+    /// <summary>
+    /// Connector to Firebase Storage
+    /// </summary>
+    public class FirestorageConnector : IFirestorageConnector
+    {
+        private readonly IOptions<FirebaseSettings> _settings;
+        private readonly ILogger _logger;        
+
+        public FirestorageConnector(IOptions<FirebaseSettings> settings, ILogger logger)
+        {
+            _settings = settings;
+            _logger = logger;
+        }
+
+        public void Initialize()
+        {
+            // Initialize Firebase
+            var firebaseOptions = new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(_settings.Value.ApiKeyFilePath),
+                ProjectId = _settings.Value.ProjectId
+            };
+            FirebaseApp.Create(firebaseOptions);            
+        }
+       
+    }
+}
