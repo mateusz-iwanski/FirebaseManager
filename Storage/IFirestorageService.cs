b248b02110dced84f3735f718a33ceaac2938dea
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-22 10:39:22 +0200

    Add IFirestorageService interface in FirebaseManager.Storage
    Introduce a new namespace `FirebaseManager.Storage` and add the
    `IFirestorageService` interface. This interface includes two
    asynchronous methods: `UploadFileAsync` for uploading files and
    `DeleteFileAsync` for deleting files.

---
+++ Storage/IFirestorageService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace FirebaseManager.Storage
+{
+    public interface IFirestorageService
+    {
+        Task UploadFileAsync(string localFilePath, string objectName);
+        Task DeleteFileAsync(string objectName);
+    }
+}
