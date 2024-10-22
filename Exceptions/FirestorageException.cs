d26cab74ba142e4652df17e2368ea1e6e4206366
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-22 10:34:23 +0200

    Add FirestorageException class to handle Firestorage errors
    Introduce FirestorageException in FirebaseManager.Exceptions.
    This class extends CustomException and includes two constructors:
    one for a message and another for a message and inner exception.
    Added necessary using directives: System, System.Collections.Generic,
    System.Linq, System.Text, System.Threading.Tasks.

---
+++ Exceptions/FirestorageException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseManager.Exceptions
+{
+    public class FirestorageException : CustomException
+    {
+        public FirestorageException(string message) : base(message)
+        {
+        }
+
+        public FirestorageException(string message, Exception innerException) : base(message, innerException) 
+        { 
+        }
+    }
+}
