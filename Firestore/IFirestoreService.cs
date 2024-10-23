4d90d782fa991c25aa3517481905ae5fd1365ccb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-23 10:42:47 +0200

    Add methods to IFirestoreService for doc check and DB access
    Two new methods were added to the `IFirestoreService` interface:
    1. `Task<bool> IsDtoExistsAsync(DocumentReference document);` - This method checks if a document exists in Firestore.
    2. `FirestoreDb GetFirestoreDb();` - This method provides access to the Firestore database instance.
    
    These additions enhance the functionality of the `IFirestoreService` interface by allowing for document existence checks and direct access to the Firestore database.

--- Firestore/IFirestoreService.cs
+++ Firestore/IFirestoreService.cs
@@ -11,5 +11,7 @@ namespace FirebaseManager.Firestore
         Task<bool> DeleteDtoAsync(DocumentReference docRef);
         Task<T> ReadDocumentAsync<T>(string collectionName, string documentUniqueField) where T : IFirestoreDto;
         Task<bool> InsertDtoWithSubDtoAsync(IFirestoreDto dto, IFirestoreDto subCollectionDto);
+        Task<bool> IsDtoExistsAsync(DocumentReference document);
+        FirestoreDb GetFirestoreDb();
     }
 }
